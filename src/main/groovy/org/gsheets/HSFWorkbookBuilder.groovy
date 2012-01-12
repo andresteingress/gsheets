61f4589d22df3f4b31821cab737d9492df8bd036
Andre Steingress <me@andresteingress.com>
2012-01-12 13:20:43 +0100

    add sheet separation

--- src/main/groovy/org/gsheets/HSFWorkbookBuilder.groovy
+++ src/main/groovy/org/gsheets/HSFWorkbookBuilder.groovy
@@ -1,3 +1,35 @@
+/**
+ Copyright (c) 2012, Andre Steingress
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+ 3. All advertising materials mentioning features or use of this software
+    must display the following acknowledgement:
+    This product includes software developed by the ASF.
+ 4. Neither the name of the ASF nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY Andre Steingress ''AS IS'' AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL Andre Steingress BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ GSheets is a Groovy builder based on Apache POI.
+ */
+
 package org.gsheets
 
 import org.apache.poi.hssf.usermodel.HSSFRichTextString
@@ -118,6 +150,8 @@ class HSSFWorkbookBuilder extends NodeBuilder {
         def fontId = args.font
         def dataFormat = args.dataFormat
 
+        def sheetName = args.sheet
+
         def rows = args.rows
         def cells = args.columns
 
@@ -129,6 +163,7 @@ class HSSFWorkbookBuilder extends NodeBuilder {
         if (cellStyleId && !cellStyles.containsKey(cellStyleId)) cellStyleId = null
         if (fontId && !fonts.containsKey(fontId)) fontId = null
         if (dataFormat && !(dataFormat instanceof String)) dataFormat = null
+        if (sheetName && !(sheetName instanceof String)) sheetName = null
 
         if (rows instanceof  Number) rows = [rows]
         if (cells instanceof  Number) cells = [cells]
@@ -138,6 +173,9 @@ class HSSFWorkbookBuilder extends NodeBuilder {
 
             cells.each { Number cellIndex ->
                 assert cellIndex
+
+                def sheet = sheetName ? workbook.getSheet(sheetName as String) : workbook.getSheetAt(0)
+                assert sheet
 
                 Row row = sheet.getRow(rowIndex.intValue() - 1)
                 if (!row) return
@@ -158,13 +196,17 @@ class HSSFWorkbookBuilder extends NodeBuilder {
     void mergeCells(Map<String, Object> args)  {
         def rows = args.rows
         def cols = args.columns
+        def sheetName = args.sheet
 
         assert rows && (rows instanceof Number || rows instanceof Range<Number>)
         assert cols && (cols instanceof Number || cols instanceof Range<Number>)
 
         if (rows instanceof Number) rows = [rows]
         if (cols instanceof Number) cols = [cols]
+        if (sheetName && !(sheetName instanceof String)) sheetName = null
 
+        def sheet = sheetName ? workbook.getSheet(sheetName as String) : workbook.getSheetAt(0)
+        
         sheet.addMergedRegion(new CellRangeAddress(rows.first() - 1, rows.last() - 1, cols.first() - 1, cols.last() - 1))
     }
 
